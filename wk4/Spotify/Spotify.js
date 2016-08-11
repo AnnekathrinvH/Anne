2f4072c8c0d8e2aee8c4c54e4156b1144ee4ad1e
AnnekathrinvH <annevonhacht@web.de>
2016-08-11 15:02:43 +0200

    working version with button

--- wk4/Spotify/Spotify.js
+++ wk4/Spotify/Spotify.js
@@ -1,85 +1,90 @@
 (function(){
+    var input;
+    var selector;
+    var data;
+    var img;
+    var name;
+    var url;
+    var choice;
+
     $("#form" ).submit(function(e) {
         $('#Container').html('');
-        var input = $('#input').val();
-        var selector = $('#select').val();
-        getData(input, selector);
+        input = $('#input').val();
+        selector = $('#select').val();
+        getData();
         e.preventDefault();
     });
 
-    function getData(a, b) {
-        $.get('https://api.spotify.com/v1/search', { q: a, type: b }, function(data) {
+
+    function getData() {
+        $.get('https://api.spotify.com/v1/search', { q: input, type: selector }, function(request) {
+            data = request;
             console.log(data);
-            console.log(b);
-            $('#searchTerm').html('Results for "'+a+'"');
-            if(b === 'artist') {
-                var img;
-                var name;
-                var url;
-                if(data.artists.items.length === 0) {
-                    $('#Container').html('no matches');
-                }
-                loadItemsArtist(data);
-
-                if(data.artists.next) {
-                    console.log('next');
-                    $('#button2').removeClass('invisible');
-                    $('#button2').click(getMoreData(data));
-                }
-            }
-            if(b === 'album') {
-                if(data.albums.items.length === 0) {
-                    $('#Container').html('no matches');
-                }
-
-                for(var i = 0; i < data.albums.items.length; i++) {
-                    var img;
-                    var name;
-                    var url;
-                    if(data.albums.items[i].images[0] === undefined) {
-                        img = 'http://www.wpclipart.com/music/performance/more_performers/marching_band.png'
-                    }
-                    else {
-                        img = data.albums.items[i].images[0].url;
-                        name = data.albums.items[i].name;
-                        url = data.albums.items[i].external_urls.spotify;
-                        console.log(name);
-                        $('#Container').append('<a href ="'+url+'"><div id="result"><img src='+img+'><a/><div id="name"><a href ="'+url+'">'+name+'<a/></div></div>');
-                    }
-                }
-                if(data.albums.next) {
-                    console.log('next');
-                    $('#button2').removeClass('invisible');
-
-                }
-            }
-            getMore(data);
+            console.log(selector);
+            showResults();
         });
+    };
 
-        function getMore(data) {
+    function showResults() {
+        $('#searchTerm').html('Results for "'+input+'"');
 
+        if(selector === 'artist') {
+            choice = data.artists;
+            if(choice.items.length === 0) {
+                $('#Container').html('no matches');
+            }
 
+            loadItems();
+
+        }
+        if(selector === 'album') {
+            choice = data.albums;
+            if(choice.items.length === 0) {
+                $('#Container').html('no matches');
+            }
+
+            loadItems();
         }
     }
 
-    function loadItemsArtist(data) {
-        for(var i = 0; i < data.artists.items.length; i++) {
-            if(data.artists.items[i].images[0] === undefined) {
+
+    function loadItems() {
+        console.log(choice);
+        for(var i = 0; i < choice.items.length; i++) {
+            if(choice.items[i].images[0] === undefined) {
                 img = 'http://www.wpclipart.com/music/performance/more_performers/marching_band.png'
             }
             else {
-                img = data.artists.items[i].images[0].url;
-                name = data.artists.items[i].name
-                url = data.artists.items[i].external_urls.spotify;
+                img = choice.items[i].images[0].url;
+                name = choice.items[i].name
+                url = choice.items[i].external_urls.spotify;
             }
+
             $('#Container').append('<a href ="'+url+'"><div id="result"><img src='+img+'><a/><div id="name"><a href ="'+url+'">'+name+'<a/></div></div>');
+        }
+        if(choice.next) {
+            console.log('next');
+            $('#button2').removeClass('invisible');
+            $('#button2').click(function() {
+                getMoreData();
+            })
+        }
+        if (choice.next === null) {
+            $('#button2').addClass('invisible');
         }
     }
 
     function getMoreData() {
-        $.get(data.artists.next, function(data2) {
-            console.log(data2);
-            loadItemsArtist(data2);
+        $.get(choice.next, function(newdata) {
+            data = newdata;
+            if(selector === 'artist') {
+                choice = data.artists;
+            }
+            if (selector === 'album') {
+                choice = data.albums;
+            }
+            console.log(data);
+            loadItems();
         });
     };
 
